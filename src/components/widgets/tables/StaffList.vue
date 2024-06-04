6135f520a710ecb0c4572bee5b08d037bcc91a85
mohamadreza <mohamadrezakhf@gmail.com>
2024-06-04 16:28:31 +0330

    fix staff page

--- src/components/widgets/tables/StaffList.vue
+++ src/components/widgets/tables/StaffList.vue
@@ -1,123 +1,126 @@
 <template>
 	<div class="card" :class="widgetClasses">
-		<div class="card-header border-0 pt-5">
-			<h3 class="card-title align-items-start flex-column">
-				<span class="card-label fw-bold fs-3 mb-1">Members Statistics</span>
+		<div class="card-header border-0 pt-5 ">
+			<div class="d-flex flex-row justify-content-between  w-100">
+				<div class="card-title">
+					<div class="d-flex align-items-center position-relative my-1">
+						<KTIcon icon-name="magnifier" icon-class="fs-1 position-absolute ms-6" />
+						<input v-model="search" type="text" class="form-control form-control-solid w-250px ps-15"
+							placeholder="Search Customers" @input="searchItems()">
+					</div>
+				</div>
+				<div class="card-toolbar " data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover">
+					<a href="#" class="btn btn-sm btn-light-primary me-2" data-bs-toggle="modal"
+						data-bs-target="#kt_modal_invite_friends">
+						<KTIcon icon-name="plus" icon-class="fs-3" />
+						Filter
+					</a>
+					<a href="#" class="btn btn-sm btn-light-primary" data-bs-toggle="modal"
+						data-bs-target="#kt_modal_invite_friends">
+						<KTIcon icon-name="plus" icon-class="fs-3" />
+						Add Staff
+					</a>
+				</div>
 
-				<span class="text-muted mt-1 fw-semibold fs-7">Over 500 members</span>
-			</h3>
-
-			<div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover"
-				title="Click to add a user">
-				<a href="#" class="btn btn-sm btn-light-primary" data-bs-toggle="modal"
-					data-bs-target="#kt_modal_invite_friends">
-					<KTIcon icon-name="plus" icon-class="fs-3" />
-					New Member
-				</a>
 			</div>
-		</div>
 
-		<div class="card-body py-3">
-			<div class="table-responsive">
-				<table class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4">
-					<thead>
-						<tr class="fw-bold text-muted">
-							<th class="w-25px">
-								<div class="form-check form-check-sm form-check-custom form-check-solid">
-									<input class="form-check-input" type="checkbox" @change="
-										checkedRows.length === 6
-											? (checkedRows.length = 0)
-											: (checkedRows = [0, 1, 2, 3, 4, 5])
-										">
-								</div>
-							</th>
-							<th class="min-w-150px">
-								User
-							</th>
-							<th class="min-w-140px">
-								Email
-							</th>
-							<th class="min-w-120px">
-								Role
-							</th>
-							<th class="min-w-100px ">
-								Last login
-							</th>
-							<th class="min-w-120px">
-								Date Joined
-							</th>
-							<th class="min-w-100px text-center">
-								Action
-							</th>
-						</tr>
-					</thead>
-
-					<tbody>
-						<template v-for="(item, index) in list" :key="index">
-							<tr>
-								<td>
+			<div class="card-body py-3">
+				<div class="table-responsive">
+					<table class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4">
+						<thead>
+							<tr class="fw-bold text-muted">
+								<th class="w-25px">
 									<div class="form-check form-check-sm form-check-custom form-check-solid">
-										<input v-model="checkedRows" class="form-check-input widget-9-check"
-											type="checkbox" :value="index">
+										<input class="form-check-input" type="checkbox" @change="
+											checkedRows.length === 6
+												? (checkedRows.length = 0)
+												: (checkedRows = [0, 1, 2, 3, 4, 5])
+											">
 									</div>
-								</td>
+								</th>
+								<th class="min-w-150px">
+									User
+								</th>
+								<th class="min-w-140px">
+									Email
+								</th>
+								<th class="min-w-120px">
+									Role
+								</th>
+								<th class="min-w-100px ">
+									Last login
+								</th>
+								<th class="min-w-120px">
+									Date Joined
+								</th>
+								<th class="min-w-100px text-center">
+									Action
+								</th>
+							</tr>
+						</thead>
 
-								<td>
-									<div class="d-flex align-items-center">
-										<div class="symbol symbol-45px me-5">
-											<img :src="item.image" alt="">
+						<tbody>
+							<template v-for="(item, index) in list" :key="index">
+								<tr>
+									<td>
+										<div class="form-check form-check-sm form-check-custom form-check-solid">
+											<input v-model="checkedRows" class="form-check-input widget-9-check"
+												type="checkbox" :value="index">
 										</div>
-										<div class="d-flex justify-content-start flex-column">
-											<a href="#" class="text-gray-900 fw-bold text-hover-primary fs-6">{{
-												item.name }}</a>
+									</td>
 
-											<span class="text-muted fw-semibold text-muted d-block fs-7">{{ item.id
-												}}</span>
+									<td>
+										<div class="d-flex align-items-center">
+											<div class="symbol symbol-45px me-5">
+												<img :src="item.image" alt="">
+											</div>
+											<div class="d-flex justify-content-start flex-column">
+												<a href="#" class="text-gray-900 fw-bold text-hover-primary fs-6">{{
+													item.name }}</a>
+
+												<span class="text-muted fw-semibold text-muted d-block fs-7">{{ item.id
+													}}</span>
+											</div>
 										</div>
-									</div>
-								</td>
+									</td>
 
-								<td>
-									<a href="#" class="text-gray-900 fw-bold text-hover-primary d-block fs-6">{{
-										item.email }}</a>
-								</td>
+									<td>
+										<a href="#" class="text-gray-900 fw-bold text-hover-primary d-block fs-6">{{
+											item.email }}</a>
+									</td>
 
-								<td class="text-end">
-									<div class="d-flex flex-column w-100 me-2">
-										<div class="d-flex flex-stack mb-2">
-											<span class=" me-2 fs-7 fw-semibold">
-												{{ item.role }}
-											</span>
+									<td class="text-end">
+										<div class="d-flex flex-column w-100 me-2">
+											<div class="d-flex flex-stack mb-2">
+												<span class=" me-2 fs-7 fw-semibold">
+													{{ item.role }}
+												</span>
+											</div>
+
+
 										</div>
+									</td>
 
-										<!-- <div class="progress h-6px w-100">
-											<div class="progress-bar" :class="`bg-${item.color}`" role="progressbar"
-												:style="{ width: item.value + '%' }" :aria-valuenow="item.value"
-												aria-valuemin="0" aria-valuemax="100" />
-										</div> -->
-									</div>
-								</td>
-
-								<td>
-									<a href="#" class="text-gray-900 fw-bold text-hover-primary d-block fs-6">{{
-										item.lastLogin }}</a>
-								</td>
+									<td>
+										<a href="#" class="text-gray-900 fw-bold text-hover-primary d-block fs-6">{{
+											item.lastLogin }}</a>
+									</td>
 
 
-								<td>
-									<a href="#" class="text-gray-900 fw-bold text-hover-primary d-block fs-6">{{
-										item.dateJoined }}</a>
-								</td>
+									<td>
+										<a href="#" class="text-gray-900 fw-bold text-hover-primary d-block fs-6">{{
+											item.dateJoined }}</a>
+									</td>
 
 
-								<td>
-									<a href="#" class="btn btn-sm btn-light-primary" data-bs-toggle="modal"
-										data-bs-target="#kt_modal_invite_friends">
-										Action
-										<KTIcon icon-name="down" icon-class="fs-3" />
-									</a>
-								</td>
-								<!-- <td class="text-end">
+									<td>
+										<a href="#" class="btn btn-sm btn-light-primary" data-bs-toggle="modal"
+											data-bs-target="#kt_modal_invite_friends">
+											Action
+											<KTIcon icon-name="down" icon-class="fs-3" />
+										</a>
+									</td>
+									<!-- <td class="text-end">
 									<a href="#" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
 										<KTIcon icon-name="switch" icon-class="fs-3" />
 									</a>
@@ -130,19 +133,21 @@
 										<KTIcon icon-name="trash" icon-class="fs-3" />
 									</a>
 								</td> -->
-							</tr>
-						</template>
-					</tbody>
-				</table>
+								</tr>
+							</template>
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
+// import { MenuComponent } from '@/assets/ts/components';
+// import type { ICustomer } from '@/core/data/customers';
 import { getAssetPath } from '@/core/helpers/assets';
 import { defineComponent, ref } from 'vue';
-
 export default defineComponent({
 	name: 'StaffList',
 	components: {},
@@ -154,7 +159,8 @@ export default defineComponent({
 	setup() {
 		const checkedRows = ref<Array<number>>([])
 
-		const list = [
+
+		const list = ref([
 			{
 				id: '#S674837',
 				image: getAssetPath('media/avatars/300-14.jpg'),
@@ -200,21 +206,45 @@ export default defineComponent({
 				lastLogin: 'Yesterday',
 				dateJoined: '2022/02/01'
 			},
-			// {
-			// 	image: getAssetPath('media/avatars/300-23.jpg'),
-			// 	name: 'Mehrshad Khalili',
-			// 	skills: 'HTML, JS, ReactJS',
-			// 	companyName: 'RoadGee',
-			// 	companySkills: 'Art Director',
-			// 	value: '90',
-			// 	color: 'info',
-			// },
-		]
+		])
+		const search = ref<string>('')
+		function searchItems() {
+			// list.value.splice(0, list.value.length, ...initCustomers.value)
+			// if (search.value !== '') {
+			// 	let results: Array<ICustomer> = []
+			// 	for (let j = 0; j < list.value.length; j++) {
+			// 		if (searchingFunc(list.value[j], search.value)) {
+			// 			results.push(list.value[j])
+			// 		}
+			// 	}
+			// 	list.value.splice(0, list.value.length, ...results)
+			// }
+			// MenuComponent.reinitialization()
+		}
+
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		function searchingFunc(obj: any, value: string): boolean {
+			for (let key in obj) {
+				if (!Number.isInteger(obj[key]) && !(typeof obj[key] === 'object')) {
+					if (obj[key].indexOf(value) != -1) {
+						return true
+					}
+				}
+			}
+			return false
+		}
+
+
+
+
 
 		return {
 			list,
 			checkedRows,
 			getAssetPath,
+			searchItems,
+			search
 		}
 	},
 })
